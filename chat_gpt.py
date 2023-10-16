42b7e1c3bd147a17381e017f2587e0024bb1e35e
eldpswp99 <nrg1392@naver.com>
2023-10-16 18:34:01 +0900

    use openai

--- chat_gpt.py
+++ chat_gpt.py
@@ -1,9 +1,6 @@
 import os
 import openai
-from langchain.agents import load_tools, initialize_agent, AgentType, create_csv_agent
-from langchain.chains import llm, LLMChain
-from langchain.chat_models import ChatOpenAI
-from langchain.llms import OpenAI
+
 
 dotenv_file = os.path.join(os.path.dirname(__file__), ".env")
 if os.path.isfile(dotenv_file):
@@ -15,84 +12,52 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
 def generate_response(type: str, subject: str):
-    passage = ""
     prompt = ""
 
     if type == "blank":
-        passage_response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You will be provided with subjects,"
-                    + " and your task is to generate SAT passage",
-                },
-                {"role": "user", "content": prompt_passage(subject, 50)},
-            ],
-        )
-        passage = passage_response["choices"][0]["message"]["content"]
-        print(passage_response)
-        print(passage)
-        prompt = prompt_blank(passage)
+        prompt = prompt_blank(subject)
     if type == "find_subject":
         prompt = prompt_find_subject(subject)
+    if type == "grammar":
+        prompt = prompt_grammar(subject)
+    if type == "conjunction":
+        prompt = prompt_conjunction(subject)
 
-    problem_response = openai.ChatCompletion.create(
+    sat_response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
             {
                 "role": "system",
-                "content": "You will be provided with passage,"
-                + " and your task is to generate SAT question and problems regarding to the passage.",
+                "content": "You will be provided with subjects, and your task is to generate SAT passage and problems.",
             },
             {"role": "user", "content": prompt},
         ],
     )
 
-    return problem_response["choices"][0]["message"]["content"]
+    return sat_response["choices"][0]["message"]["content"]
 
 
-def prompt_passage(subject: str, words_count: int):
-    return f"""
-    ⌨️ 
-
-    Step 0.
-    Topic : {subject}
-
-    Step 1.
-    Please Generate new passage about {subject} from journals, research papers, etc
-    The paragraph length must be about {words_count} words 
-    The paragraph must be at most {words_count + 10} words
-    The difficulty of the passage should be fit for SAT."""
-
-
-def prompt_blank(passage: str):
+def prompt_blank(subject: str):
     return f"""
 ⌨️ You will be provided with subjects, and your task is to generate SAT passage and problems.
 
+Step 0.
+Topic : {subject}
+
 Step 1.
-Use the passage below.
-{passage}
+Please Generate new passage about subject from journals, research papers, etc
+The paragraph length must be about 50 words
 
 Step 2.
 Generate a "Fill in the blank word finding exercise" problem from the passage.
 Choose a keyword verb, noun, adverb, or adjective from the passage, and exchange with blank.
+And print the whole passage with blank.
 The blank word should be one word.
 The answer must be inferable from the context.
 The format of problem should be same as Example.
 
 Step 3.
-Return in the Format
-Passage
-
-Question
-
-A. Choice 1
-B. Choice 2
-C. Choice 3
-D. Choice 4
-
-Answer
+Show what is the answer.
 
 - Example 1.
 
@@ -154,26 +119,17 @@ def prompt_find_subject(subject: str):
     return f"""
 ⌨️ You will be provided with subjects, and your task is to generate SAT passage and problems.
 
-Step 0.
-Topic : {subject}
+Topic : `Ancient earth`
 
 Step 1.
-Please find a passage about {subject} from journals, research papers, etc from the internet.
-The difficulty of the passage should be fit for SAT.
+Please Generate new passage about subject from journals, research papers, etc
 The paragraph length must be about 100 words
 
 Step 2.
-Generate a "Find subject" problem from the passage.
-And print the whole passage.
-The question should be "Which choice best states the main purpose of the
-text?". or Which choice best describes the function of the
-underlined sentence in the text as a whole? or Which choice best states the main purpose of the
-text?. 
-The problem is in multiple choices with A,B,C,D.
+Generate a problem from the passage.
+Problem type must be similar with examples.
 The answer must be inferable from the context.
-The problems should be relative to the passage and question.
 The format of problem should be same as Example.
-
 
 Step 3.
 Show what is the answer.
@@ -251,4 +207,155 @@ C) To call attention to a certain writer’s careful and
 elaborately detailed writing process
 D) To recount fond memories of an afternoon spent
 in nature with a certain write
+"""
+
+
+def prompt_grammar(subject: str):
+    return f"""
+⌨️ You will be provided with subjects, and your task is to generate SAT passage and problems.
+
+Topic : {subject}
+
+Step 1.
+Please Generate new passage about subject from journals, research papers, etc
+The paragraph length must be about 50 words
+
+Step 2.
+Generate a grammar exercise from the passage.
+First, choose a verb from the passage and make options with the verb's other tense and form so that there exists only one grammatically correct answer.
+Second, Replace the chosen verb with blank.
+Last, Print the whole passage with blank.
+
+The chosen verb tense should not be trivial.
+Problem type must be similar with examples.
+The answer must be inferable from the context.
+The format of problem should be same as Example.
+
+Step 3.
+Show what is the answer.
+
+- Example 1.
+
+In ancient Greece, an Epicurean was a follower of
+Epicurus, a philosopher whose beliefs revolved
+around the pursuit of pleasure. Epicurus defined
+pleasure as “the absence of pain in the body and of
+trouble in the _______ that all life’s virtues derived
+from this absence.
+Which choice completes the text so that it conforms
+to the conventions of Standard English?
+A) soul,” positing
+B) soul”: positing
+C) soul”; positing
+D) soul.” Positing
+
+- Example 2.
+
+British scientists James Watson and Francis Crick
+won the Nobel Prize in part for their 1953 paper
+announcing the double helix structure of DNA, but it
+is misleading to say that Watson and Crick
+discovered the double helix. _______ findings were
+based on a famous X-ray image of DNA fibers,
+“Photo 51,” developed by X-ray crystallographer
+Rosalind Franklin and her graduate student
+Raymond Gosling.
+Which choice completes the text so that it conforms
+to the conventions of Standard English?
+A) They’re
+B) It’s
+C) Their
+D) Its
+
+- Example 3.
+
+In 1637, the price of tulips skyrocketed in
+Amsterdam, with single bulbs of rare varieties
+selling for up to the equivalent of $200,000 in today’s
+US dollars. Some historians _______ that this “tulip
+mania” was the first historical instance of an asset
+bubble, which occurs when investors drive prices to
+highs not supported by actual demand.
+Which choice completes the text so that it conforms
+to the conventions of Standard English?
+A) claiming
+B) claim
+C) having claimed
+D) to claim
+"""
+
+
+def prompt_conjunction(subject: str):
+    return f"""
+⌨️ You will be provided with subjects, and your task is to generate SAT passage and problems.
+
+Topic : {subject}
+
+Step 1.
+Please Generate new passage about subject from journals, research papers, etc
+The paragraph length must be about 50 words
+Show the whole paragraph
+
+Step 2.
+Generate a "Find the correct conjunctions" exercise from the passage.
+First, choose a conjunction from the passage and make options with other conjunctions so that there exists only one correct answer.
+Second, Replace the chosen conjunction with blank.
+Last, Print the whole passage with blank.
+
+Problem type must be similar with examples.
+The answer must be inferable from the context.
+The format of problem should be same as Example.
+
+Step 3.
+Show what is the answer.
+
+- Example 1.
+
+Although novels and poems are considered distinct
+literary forms, many authors have created hybrid
+works that incorporate elements of both. Bernardine
+Evaristo’s The Emperor's Babe, _______ is a verse
+novel, a book-length narrative complete with
+characters and a plot but conveyed in short, crisp
+lines of poetry rather than prose.
+Which choice completes the text with the most
+logical transition?
+A) by contrast,
+B) consequently,
+C) secondly,
+D) for example,
+
+- Example 2.
+
+At two weeks old, the time their critical socialization
+period begins, wolves can smell but cannot yet see or
+hear. Domesticated dogs, _______ can see, hear, and
+smell by the end of two weeks. This relative lack of
+sensory input may help explain why wolves behave
+so differently around humans than dogs do: from a
+very young age, wolves are more wary and less
+exploratory.
+Which choice completes the text with the most
+logical transition?
+A) in other words,
+B) for instance,
+C) by contrast,
+D) accordingly
+
+- Example 3.
+
+Researchers Helena Mihaljević-Brandt, Lucía
+Santamaría, and Marco Tullney report that while
+mathematicians may have traditionally worked
+alone, evidence points to a shift in the opposite
+direction. _______ mathematicians are choosing to
+collaborate with their peers—a trend illustrated by a
+rise in the number of mathematics publications
+credited to multiple authors.
+Which choice completes the text with the most
+logical transition?
+A) Similarly,
+B) For this reason,
+C) Furthermore,
+D) Increasingly,
 """
